01554785d34f5b0c5956ec9a67931875f9bf6e08
tangyi <1633736729@qq.com>
2020-02-23 17:15:29 +0800

    优化

--- docs/deploy/mysql/init/microservice_gateway.sql
+++ docs/deploy/mysql/init/microservice_gateway.sql
@@ -1,5 +1,4 @@
 
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
@@ -28,7 +27,7 @@ CREATE TABLE `sys_route`  (
 -- ----------------------------
 -- Records of sys_route
 -- ----------------------------
-INSERT INTO `sys_route` VALUES (607150228717572096, 'msc-service', '消息中心', '[{\"name\":\"Path\",\"args\":{\"_genkey_0\":\"/api/msc/**\"}}]', '[{\"name\":\"StripPrefix\",\"args\":{\"_genkey_0\":\"2\"}},{\"name\":\"RemoveRequestHeader\",\"args\":{\"_genkey_0\":\"Cookie\",\"_genkey_1\":\"Set-Cookie\"}}]', 'lb://msc-service', 4, 0, '', '2020-02-05 11:51:41', '', '2020-02-05 11:51:42', 0, 'EXAM');
+INSERT INTO `sys_route` VALUES (607150228717572096, 'msc-service', '消息中心', '[{\"name\":\"Path\",\"args\":{\"_genkey_0\":\"/api/msc/**\"}}]', '[{\"name\":\"StripPrefix\",\"args\":{\"_genkey_0\":\"2\"}},{\"name\":\"RemoveRequestHeader\",\"args\":{\"_genkey_0\":\"Cookie\",\"_genkey_1\":\"Set-Cookie\"}}]', 'lb://msc-service', 4, 0, 'admin', '2020-02-23 14:42:05', '', '2020-02-05 11:51:42', 0, 'EXAM');
 INSERT INTO `sys_route` VALUES (607150228717572097, 'user-service', '用户服务', '[{\"name\": \"Path\",\"args\": {\"_genkey_0\": \"/api/user/**\"}}]', '[\n  {\n    \"name\": \"StripPrefix\",\n    \"args\": {\n      \"_genkey_0\": \"2\"\n    }\n  },\n  {\n    \"name\": \"RemoveRequestHeader\",\n    \"args\": {\n      \"_genkey_0\": \"Cookie\",\n      \"_genkey_1\": \"Set-Cookie\"\n    }\n  }\n]', 'lb://user-service', 2, 0, 'admin', '2020-02-04 14:15:08', '', '2019-08-03 09:58:35', 0, 'EXAM');
 INSERT INTO `sys_route` VALUES (607150228717572098, 'exam-service', '考试服务', '[{\"name\":\"Path\",\"args\":{\"_genkey_0\":\"/api/exam/**\"}}]', '[{\"name\":\"StripPrefix\",\"args\":{\"_genkey_0\":\"2\"}},{\"name\":\"RemoveRequestHeader\",\"args\":{\"_genkey_0\":\"Cookie\",\"_genkey_1\":\"Set-Cookie\"}}]', 'lb://exam-service', 3, 0, 'admin', '2020-02-05 11:52:19', '', '2020-02-05 11:52:19', 0, 'EXAM');
 INSERT INTO `sys_route` VALUES (607150228717572099, 'auth-service', '认证授权服务', '[{\"name\":\"Path\",\"args\":{\"_genkey_0\":\"/api/auth/**\"}}]', '[\n  {\n    \"name\": \"StripPrefix\",\n    \"args\": {\n      \"_genkey_0\": \"2\"\n    }\n  },\n  {\n    \"name\": \"RemoveRequestHeader\",\n    \"args\": {\n      \"_genkey_0\": \"Cookie\",\n      \"_genkey_1\": \"Set-Cookie\"\n    }\n  }\n]', 'lb://auth-service', 1, 0, 'admin', '2020-02-04 14:17:04', '', '2020-02-04 14:17:05', 0, 'EXAM');
