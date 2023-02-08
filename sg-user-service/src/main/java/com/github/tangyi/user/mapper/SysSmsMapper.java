fc58d8e396d5a9dc5104bfa173d6be59a27e119f
慕容若冰 <1633736729@qq.com>
2023-02-08 22:09:35 +0800

    发送短信优化

---
+++ sg-user-service/src/main/java/com/github/tangyi/user/mapper/SysSmsMapper.java
@@ -0,0 +1,9 @@
+package com.github.tangyi.exam.mapper;
+
+import com.github.tangyi.api.exam.model.SysSms;
+import com.github.tangyi.common.base.CrudMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SysSmsMapper extends CrudMapper<SysSms> {
+}
---
+++ sg-user-service/src/test/java/com/github/tangyi/user/SmsTests.java
@@ -0,0 +1,7 @@
+package com.github.tangyi.user;/**
+ * 
+ *
+ * @author tangyi
+ * @date 2023/2/8 13:20
+ */public class SmsTests {
+}
