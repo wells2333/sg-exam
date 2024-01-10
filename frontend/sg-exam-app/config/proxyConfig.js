d66d092a75ec9faa3710d113ad9a6855938c4c6c
lizp <1305177077@qq.com>
2024-01-10 21:53:48 +0800

    feat: 优化本地开发时的proxy代理转发;

--- frontend/sg-exam-app/config/proxyConfig.js
+++ frontend/sg-exam-app/config/proxyConfig.js
@@ -1,28 +1,14 @@
-// user服务地址
-const USER_SERVICE_HOST = process.env.USER_SERVICE_HOST || 'localhost'
-const USER_SERVICE_PORT = process.env.USER_SERVICE_PORT || '9183'
+const dotenv = require('dotenv');
+dotenv.config();
 
-// auth服务地址
-const AUTH_SERVICE_HOST = process.env.AUTH_SERVICE_HOST || 'localhost'
-const AUTH_SERVICE_PORT = process.env.AUTH_SERVICE_PORT || '9182'
-
-// exam服务地址
-const EXAM_SERVICE_HOST = process.env.EXAM_SERVICE_HOST || 'localhost'
-const EXAM_SERVICE_PORT = process.env.EXAM_SERVICE_PORT || '9184'
+const USER_SERVICE_HOST = process.env.USER_SERVICE_HOST
+const USER_SERVICE_PORT = process.env.USER_SERVICE_PORT
 
 // 转发配置
 module.exports = {
   proxyList: {
     '/sg-user-service': {
       target: 'http://' + USER_SERVICE_HOST + ':' + USER_SERVICE_PORT,
-      changeOrigin: true
-    },
-    '/sg-exam-service': {
-      target: 'http://' + EXAM_SERVICE_HOST + ':' + EXAM_SERVICE_PORT,
-      changeOrigin: true
-    },
-    '/sg-auth-service': {
-      target: 'http://' + AUTH_SERVICE_HOST + ':' + AUTH_SERVICE_PORT,
       changeOrigin: true
     }
   }
