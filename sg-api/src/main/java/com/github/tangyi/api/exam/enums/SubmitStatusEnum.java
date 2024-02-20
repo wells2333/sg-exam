de5f6a1917e04f66273c965affac33c8e925c0f6
慕容若冰 <1633736729@qq.com>
2024-02-20 21:51:20 +0800

    improvement: 优化成绩导出

--- sg-api/src/main/java/com/github/tangyi/api/exam/enums/SubmitStatusEnum.java
+++ sg-api/src/main/java/com/github/tangyi/api/exam/enums/SubmitStatusEnum.java
@@ -2,44 +2,53 @@ package com.github.tangyi.api.exam.enums;
 
 public enum SubmitStatusEnum {
 
-    NOT_SUBMITTED("未提交", 0),
-    SUBMITTED("已提交", 1),
-    CALCULATE("正在统计", 2),
-    CALCULATED("统计完成", 3);
+	UNKNOWN("未知", -1), NOT_SUBMITTED("未提交", 0), SUBMITTED("已提交", 1), CALCULATE("正在统计", 2), CALCULATED(
+			"统计完成", 3);
 
-    private String name;
+	private String name;
 
-    private Integer value;
+	private Integer value;
 
-    SubmitStatusEnum(String name, Integer value) {
-        this.name = name;
-        this.value = value;
-    }
+	SubmitStatusEnum(String name, Integer value) {
+		this.name = name;
+		this.value = value;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public Integer getValue() {
-        return value;
-    }
+	public Integer getValue() {
+		return value;
+	}
 
-    public void setValue(Integer value) {
-        this.value = value;
-    }
+	public void setValue(Integer value) {
+		this.value = value;
+	}
 
-    public static SubmitStatusEnum match(Integer value, SubmitStatusEnum defaultValue) {
-        if (value != null) {
-            for (SubmitStatusEnum item : SubmitStatusEnum.values()) {
-                if (item.value.equals(value)) {
-                    return item;
-                }
-            }
-        }
-        return defaultValue;
-    }
+	public static SubmitStatusEnum match(Integer value, SubmitStatusEnum defaultValue) {
+		if (value != null) {
+			for (SubmitStatusEnum item : SubmitStatusEnum.values()) {
+				if (item.value.equals(value)) {
+					return item;
+				}
+			}
+		}
+		return defaultValue;
+	}
+
+	public static SubmitStatusEnum matchByName(String name, SubmitStatusEnum defaultValue) {
+		if (name != null) {
+			for (SubmitStatusEnum item : SubmitStatusEnum.values()) {
+				if (item.name.equals(name)) {
+					return item;
+				}
+			}
+		}
+		return defaultValue;
+	}
 }
