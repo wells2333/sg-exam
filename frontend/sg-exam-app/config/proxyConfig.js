86def8ae4b087af37155f21f2099b6f04ad79c20
lizp <1305177077@qq.com>
2024-03-10 18:10:09 +0800

    feat: 修改本地proxy配置;

--- frontend/sg-exam-app/config/proxyConfig.js
+++ frontend/sg-exam-app/config/proxyConfig.js
@@ -1,11 +1,12 @@
 const USER_SERVICE_HOST = process.env.USER_SERVICE_HOST
 const USER_SERVICE_PORT = process.env.USER_SERVICE_PORT
 
+
 // 转发配置
 module.exports = {
   proxyList: {
     '/sg-user-service': {
-      target: 'http://' + USER_SERVICE_HOST + ':' + USER_SERVICE_PORT,
+      target: USER_SERVICE_PORT === '80' ? `https://${USER_SERVICE_HOST}`: `http://${USER_SERVICE_HOST}:${USER_SERVICE_PORT}`,
       changeOrigin: true
     }
   }
