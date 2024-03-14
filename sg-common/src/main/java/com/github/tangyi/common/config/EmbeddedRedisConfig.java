a3371467f91493ddba85d925e013f2f36ce0db3a
慕容若冰 <1633736729@qq.com>
2024-03-14 22:39:08 +0800

    feat: 支持禁用内嵌 Redis

--- sg-common/src/main/java/com/github/tangyi/common/config/EmbeddedRedisConfig.java
+++ sg-common/src/main/java/com/github/tangyi/common/config/EmbeddedRedisConfig.java
@@ -1,5 +1,6 @@
 package com.github.tangyi.common.config;
 
+import com.github.tangyi.common.utils.EnvUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -12,30 +13,38 @@ import javax.annotation.PostConstruct;
 @Configuration
 public class EmbeddedRedisConfig {
 
-    private RedisServer redis;
+	// 默认启用内嵌 Redis，可通过配置环境变量 ENABLE_EMBEDDED_REDIS=false 禁用
+	private static final Boolean ENABLE_EMBEDDED_REDIS = Boolean.valueOf(
+			EnvUtils.getValue("ENABLE_EMBEDDED_REDIS", "true"));
 
-    @PostConstruct
-    public void start() {
-        try {
-            log.info("Starting embedded redis...");
-            this.redis = RedisServer.builder()
-                    .port(6379)
-                    .setting("bind 127.0.0.1")
-                    .setting("daemonize no")
-                    .setting("appendonly no")
-                    .setting("maxmemory 128M")
-                    .build();
-            this.redis.start();
-            log.info("Embedded redis started successfully.");
-        } catch (Exception e) {
-            log.error("Failed to start embedded redis.", e);
-        }
+	private RedisServer redis;
 
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            if (redis != null) {
-                redis.stop();
-                log.info("Embedded redis stopped successfully.");
-            }
-        }));
-    }
+	@PostConstruct
+	public void start() {
+		if (!ENABLE_EMBEDDED_REDIS) {
+			return;
+		}
+
+		try {
+			log.info("Starting embedded redis...");
+			this.redis = RedisServer.builder()    //
+					.port(6379)    //
+					.setting("bind 127.0.0.1")    //
+					.setting("daemonize no")    //
+					.setting("appendonly no")    //
+					.setting("maxmemory 128M")    //
+					.build();    //
+			this.redis.start();
+			log.info("Embedded redis started successfully.");
+		} catch (Exception e) {
+			log.error("Failed to start embedded redis.", e);
+		}
+
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+			if (redis != null) {
+				redis.stop();
+				log.info("Embedded redis stopped successfully.");
+			}
+		}));
+	}
 }
